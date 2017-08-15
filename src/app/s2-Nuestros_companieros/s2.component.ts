9f34e42891df805b8c98bee74242f5c168d35ede
danimanrique <daniela.manrique22@hotmail.com>
2017-08-15 09:24:34 -0300

    s2 responsive

--- src/app/s2-Nuestros_companieros/s2.component.ts
+++ src/app/s2-Nuestros_companieros/s2.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation, NgxGalleryImageSize } from 'ngx-gallery';
 import { Afiche } from '../afiche';
 import { AppService } from '../app.service'
 
@@ -27,12 +27,35 @@ export class Seccion2Component implements OnInit{
                 width: "80%",
                 thumbnailsColumns: 6,
                 thumbnailsRows: 2,
-                thumbnailMargin: 3,
+                thumbnailMargin: 2,
                 thumbnailsArrowsAutoHide: true,
                 previewCloseOnClick: true,
-                previewKeyboardNavigation: true
+                previewKeyboardNavigation: true,
+                thumbnailSize:  NgxGalleryImageSize.Contain
+            },
+            //1349
+            {
+                breakpoint: 1349,
+                height: '300px',
+                width: "80%",
+                thumbnailsColumns: 5
+            },
+            // 1072
+            {
+                breakpoint: 1072,
+                height: '300px',
+                width: "80%",
+                thumbnailsColumns: 4
+            },
+            // 700
+            {
+                breakpoint: 700,
+                image: false,
+                height:'100px',
+                width: "80%",
+                thumbnailsColumns: 4,
+                thumbnailsRows: 1
             }
-            //1366 - 600
         ];
 
         this._compaService.getCompas().subscribe(
