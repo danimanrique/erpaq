b61950f1fb4787b72a62c373caf813368a96fe3d
eckolosst <lucasp.1606@gmail.com>
2017-08-30 15:44:40 -0300

    Comenzada vista de busquedas

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -8,8 +8,45 @@ declare var $: any;
 })
 
 export class SearchComponent implements OnInit {
+    public s1Press:boolean;
+    public s2Press:boolean;
+    public s3Press:boolean;
+    public s4Press:boolean;
+    public s5Press:boolean;
+    public all:boolean;
 
     ngOnInit() {
-
+        this.s1Press = false;
     }
+    
+    public activarBoton(valor:number): void {
+        switch(valor){
+            case(1):
+            this.s1Press = !this.s1Press;
+            break;
+            case(2):
+            this.s2Press = !this.s2Press;
+            break;
+            case(3):
+            this.s3Press = !this.s3Press;
+            break;
+            case(4):
+            this.s4Press = !this.s4Press;
+            break;
+            case(5):
+            this.s5Press = !this.s5Press;
+            break;
+            case(0):
+            this.s1Press = !this.s1Press;
+            this.s2Press = !this.s2Press;
+            this.s3Press = !this.s3Press;
+            this.s4Press = !this.s4Press;
+            this.s5Press = !this.s5Press;
+            break;
+            default:
+            console.log('Error interno en switch')
+            break;
+        }
+    }
+
 }
