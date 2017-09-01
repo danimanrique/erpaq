c26e02e8f151319bdae95c1dd60ea2bf00f7de0e
eckolosst <lucasp.1606@gmail.com>
2017-09-01 15:57:48 -0300

    ultimo intento arreglo juicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,8 @@ import { HomeComponent } from './home/home.component';
 import { Seccion1Component } from './s1-El_estado_terrorista/s1.component';
 import { Seccion2Component } from './s2-Nuestros_companieros/s2.component';
 import { Seccion3Component } from './s3-Todos_contra_la_impunidad/s3.component';
-import { Seccion4Component } from './s4-Juicios_Bahia_y_Nqn/s4.component';
+import { Seccion4Component } from './s4-Juicios_Bahia/s4.component';
+import { Seccion5Component } from './s5-Juicios_Nqn/s5.component';
 import { SearchComponent } from './search/search.component';
 import { ErrorComponent } from './error/error.component';
 
@@ -24,6 +25,7 @@ import { NgxGalleryModule } from 'ngx-gallery';
       Seccion2Component,
       Seccion3Component,
       Seccion4Component,
+      Seccion5Component,
       SearchComponent,
       ErrorComponent
    ],
---
+++ src/app/s4-Juicios_Bahia/s4.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation, NgxGalleryImageSize} from 'ngx-gallery';
+import { Afiche } from '../afiche';
+import { AppService } from '../app.service';
+
+
+@Component({
+   selector: 'seccion4',
+   templateUrl: 's4.template.html',
+   providers: [AppService]
+
+})
+
+export class Seccion4Component implements OnInit{
+    public galleryOptions: NgxGalleryOptions[];
+    public galleryImages: NgxGalleryImage[];
+
+    constructor(
+        private _appService:AppService,
+    ){}
+
+    ngOnInit():void{
+        this.galleryOptions = [
+            {
+                width: '100%',
+                height: '800px',
+                imagePercent: 85,
+                imageSize: NgxGalleryImageSize.Contain,
+                thumbnailsColumns: 6,
+                imageAnimation: NgxGalleryAnimation.Slide,
+                thumbnailsPercent: 15,
+                previewDescription: true,
+                previewCloseOnClick: true,
+                previewKeyboardNavigation: true
+            },
+            // max-width 1366
+            {
+                breakpoint: 1366,
+                height: '600px',
+                imageSize: NgxGalleryImageSize.Contain,
+                thumbnailsColumns: 6,
+                imageAnimation: NgxGalleryAnimation.Slide,
+                thumbnailsPercent: 10,
+                previewDescription: true,
+                thumbnailsMargin:2
+            },
+            // max-width 600
+            {
+                breakpoint:600,
+                image: false,
+                height:'150px',
+                thumbnailsColumns: 4,
+                thumbnailsPercent: 20,
+                previewDescription: true,
+            }
+        ];
+        this._appService.getBahia().subscribe(
+            result =>{
+                this.galleryImages = result.afiches;
+            },
+            error =>{
+                this.galleryImages = [];
+                console.log(<any>error);
+            }
+        );
+    }
+}
